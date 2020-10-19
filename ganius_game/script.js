b1b558554f4f014d1d8620bec2c89b9fe77d22bb
Eric Alberto Costa e Costa <71726316+ericcostac@users.noreply.github.com>
2020-10-19 19:37:40 -0300

    Reajustando code

--- ganius_game/script.js
+++ ganius_game/script.js
@@ -1,6 +1,6 @@
 let order = [];
-let clicketOrder = [];
-let scare = 0;
+let clickedOrder = [];
+let score = 0;
 
 //verde = 0
 //vermelho = 1
@@ -16,24 +16,100 @@ const yellow = document.querySelector('.yellow');
 const green = document.querySelector('.green');
 
 
-//FUNÇÃO DE GERAR ORDEM 
+//FUNÇÃO DE GERAR ORDEM DAS CORES
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
        
     //ATIBUIÇÃO ORDENADA
     order[order.length] = colorOrder;
-    clicketOrder = [];
+    clickedOrder = [];
 
     for(let i in order) {
-         let elementColor = createElement(order[i]);
-         lightColor(elementColor, Number(i) * 1)
+         let elementColor = createColorElement(order[i]);
+         lightColor(elementColor, Number(i) + 1);
 
      }
 }
-
+//ACENDE A PROX COR     
 let lightColor = (element, number) => {
-    time = time * 500;
+    number = time * 500;
     setTimeout(() =>{
+        element.classList.add('selected');
+    }, number - 250);
 
-    }, tempo);
+    setTimeout(() =>{
+        element.classList.remove('selected');
+    });
 }
+
+//CHECANDO A ORDENAÇÃO DAS CORES
+
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
+        }
+    }
+
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
+}
+
+//FUNÇÃO PARA CLICK DO USER 
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    }, 250);
+    
+}
+//FUNÇÃO QUE RETORNA A COR 
+
+let createColorElement = (color) => {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if(color == 2) {
+        return yellow;
+    } else if(color == 3){
+        return blue;
+    }
+}
+
+//FUNÇÃO PROX NIVEL
+
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+//FUNÇÃO GAME OVER
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+
+}
+
+let playGame = () => {
+    alert('Bem vindo ao GENIUS! Iniciando novo jogo!');
+    score = 0;
+
+    nextLevel();
+}
+
+green.addEventListener('click', click(0));
+red.addEventListener('click', click(1));
+yellow.addEventListener('click', click(2));
+blue.addEventListener('click', click(3));
+
+playGame();
